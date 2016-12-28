c4c69bbebaa9fc43fe97cc2f535b2fd4b2393cd6
Stephen DuVall <stephen.duvall13@gmail.com>
2016-12-28 16:40:35 -0500

    Started OOP Python
    Began object oriendted python coding from a video on youtube

---
+++ employee.py
@@ -0,0 +1,41 @@
+class Employee:
+
+    num_of_emps = 0
+    raise_amt = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+        Employee.num_of_emps += 1
+
+    def fullname(self):
+        return '{0} {1}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * Employee.raise_ammt)
+
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amt = amount
+
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
+
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
+
+emp_1 = Employee('Stephen', 'DuVall', 70000)
+emp_2 = Employee('Test', 'User', 60000)
+
+import datetime
+my_date = datetime.date(2016, 12, 28)
+
+print(Employee.is_workday(my_date))
